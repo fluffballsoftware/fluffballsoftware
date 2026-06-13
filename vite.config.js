da7b3f9fff54a06ea95dac798845f8d27829cbf8
Russell Gordon <russell75uk@yahoo.co.uk>
2026-06-13 05:53:46 +0100

    fix broken cookie and privacy links

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        privacy: resolve(__dirname, 'privacy.html'),
+        cookiePolicy: resolve(__dirname, 'cookie-policy.html')
+      }
+    }
+  }
 })
